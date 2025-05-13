1cc777c0d1295a09abd3ee9a6c4e5a22ee851e43
Sharmaryan <panditaryan.as@gmail.com>
2025-05-13 12:34:07 +0530

    feat : moved common files to utils

---
+++ backend/src/utils/auth.utils.ts
@@ -0,0 +1,9 @@
+
+import { encodeBase64 } from 'hono/utils/encode'
+
+export async function hashPassword(password: string): Promise<string> {
+	const encoder = new TextEncoder()
+	const passwordBuffer = encoder.encode(password)
+	const hashBuffer = await crypto.subtle.digest('SHA-256', passwordBuffer)
+	return encodeBase64(new Uint8Array(hashBuffer))
+}
