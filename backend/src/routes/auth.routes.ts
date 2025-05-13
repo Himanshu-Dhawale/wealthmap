19f332fc03f877b2b3332216ab90fbd25bc962d7
Sharmaryan <panditaryan.as@gmail.com>
2025-05-13 21:56:44 +0530

    chore : format auth file

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,11 +1,11 @@
-import { Hono } from "hono";
-import { registerCompany, login } from "../controllers/auth.controller";
+import { Hono } from 'hono';
+import { registerCompany, login } from '../controllers/auth.controller';
 
 export const authRouter = new Hono<{
-  Bindings: {
-    DATABASE_URL: string;
-    JWT_SECRET: string;
-  }
+	Bindings: {
+		DATABASE_URL: string;
+		JWT_SECRET: string;
+	};
 }>();
 
 authRouter.post('/register', registerCompany);
