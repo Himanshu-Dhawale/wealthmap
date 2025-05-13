88c3450d6970ff01330e114db3b1f005ad290a4e
Sharmaryan <panditaryan.as@gmail.com>
2025-05-13 17:29:13 +0530

    chore : migrated

---
+++ backend/prisma/migrations/20250513113951_company_user_new_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Company" ADD COLUMN     "location" TEXT,
+ADD COLUMN     "size" TEXT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firstName" TEXT,
+ADD COLUMN     "lastName" TEXT;
