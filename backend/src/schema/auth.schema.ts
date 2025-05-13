9c01cc73cc7009e70aab3aae4cbaf0b7b448d6a4
Sharmaryan <panditaryan.as@gmail.com>
2025-05-13 12:34:36 +0530

    feat : auth scheam

---
+++ backend/src/schema/auth.schema.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+const passwordSchema = z.string()
+	.min(12, { message: 'Password must be at least 12 characters long' })
+	.refine((value) => /[A-Z]/.test(value), {
+		message: 'Password must contain at least one uppercase letter',
+	}).refine((value) => /[a-z]/.test(value), {
+		message: 'Password must contain at least one lowercase letter',
+	})
+	.refine((value) => /[0-9]/.test(value), {
+		message: 'Password must contain at least one number',
+	})
+	.refine((value) => /[^A-Za-z0-9]/.test(value), {
+		message: 'Password must contain at least one special character',
+	})
+
+
+export const RegisterSchema = z.object({
+	name: z.string().min(2, 'Company name must be at least 2 characters'),
+	email: z.string().email('Invalid email address'),
+	password: passwordSchema
+})
+
+export const LoginSchema = z.object({
+	email: z.string().email('Invalid email address'),
+	password: z.string().min(1, 'Password is required')
+})
+
+
