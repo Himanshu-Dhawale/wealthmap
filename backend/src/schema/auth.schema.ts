93b6758ef3858929d939829e7f8129d2b0a2cb95
Sharmaryan <panditaryan.as@gmail.com>
2025-05-13 17:30:08 +0530

    reafactor : updated validation rules

--- backend/src/schema/auth.schema.ts
+++ backend/src/schema/auth.schema.ts
@@ -18,7 +18,12 @@ const passwordSchema = z.string()
 export const RegisterSchema = z.object({
 	name: z.string().min(2, 'Company name must be at least 2 characters'),
 	email: z.string().email('Invalid email address'),
-	password: passwordSchema
+	password: passwordSchema,
+	logo: z.string().min(1, "Logo is required"),
+	firstName: z.string().min(2, 'First name must be at least 4 characters'),
+	lastName: z.string().min(2, 'Last name must be at least 2 characters'),
+	size: z.string().min(1, 'Size is required'),
+	location: z.string().min(1, 'Location is required')
 })
 
 export const LoginSchema = z.object({
