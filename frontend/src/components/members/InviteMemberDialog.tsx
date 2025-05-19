9258d1bf1ffdcc8a68fc5867679fa659b1bafad6
chanchal16 <rajputchanchal17@gmail.com>
2025-05-19 22:55:06 +0530

    feat: create member card and invite member components

---
+++ frontend/src/components/members/InviteMemberDialog.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { useState } from "react";
+import { useMembersStore } from "@/stores/membersStore";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Mail } from "lucide-react";
+import { toast } from "sonner";
+import { useSession } from "next-auth/react";
+
+export function InviteMemberDialog() {
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState<"admin" | "member">("member");
+  const [isLoading, setIsLoading] = useState(false);
+  const { data: session } = useSession();
+  console.log("session", session);
+  const inviteMember = useMembersStore((state) => state.inviteMember);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      inviteMember(email, role);
+      // toast.success('Invitation sent',{
+      //   description: `An invitation has been sent to ${email}`,
+      // });
+      setEmail("");
+      setRole("member");
+      setOpen(false);
+    } catch (error) {
+      toast.error("Error", {
+        description: "Failed to send invitation",
+      });
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="default">Invite Member</Button>
+      </DialogTrigger>
+      <DialogContent className="bg-white">
+        <DialogHeader>
+          <DialogTitle>Invite New Member</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-4 bg">
+          <div className="space-y-2">
+            <label className="text-sm font-medium leading-none">Email</label>
+            <div className="relative">
+              <Mail className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+              <Input
+                type="email"
+                placeholder="member@example.com"
+                className="pl-10"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <Button
+            variant={"default"}
+            type="submit"
+            className="w-full bg-blue-gradient-start"
+            disabled={isLoading}
+          >
+            {isLoading ? "Sending..." : "Send Invitation"}
+          </Button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
