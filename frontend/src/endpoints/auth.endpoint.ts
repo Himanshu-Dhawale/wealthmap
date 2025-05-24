e6987cdaf1a97b596cf8f2d950bdcb57aaa41e24
chanchal16 <rajputchanchal17@gmail.com>
2025-05-24 19:06:27 +0530

    update: add 2FA endpoints

--- frontend/src/endpoints/auth.endpoint.ts
+++ frontend/src/endpoints/auth.endpoint.ts
@@ -1,2 +1,4 @@
-export const SIGNIN = '/auth/login'
-export const SIGNUP = '/auth/register'
+export const SIGNIN = "/auth/login";
+export const SIGNUP = "/auth/register";
+export const MFA = "auth/mfa/setup";
+export const VERIFY_MFA = "auth/mfa/verify";
