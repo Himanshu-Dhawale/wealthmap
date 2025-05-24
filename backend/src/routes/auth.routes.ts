3bf4b925b0d13797717c44205c8cf98dd33ee1f6
Sharmaryan <panditaryan.as@gmail.com>
2025-05-24 13:12:12 +0530

    chore : importing middleware

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono';
 import { registerCompany, login, setupMfa, verifyMfaSetup } from '../controllers/auth.controller';
 import { verifyToken } from '../middleware/auth.middleware';
+import { onlyEmployee } from '../middleware/only-employee.middleware';
 
 export const authRouter = new Hono<{
 	Bindings: {
@@ -11,5 +12,5 @@ export const authRouter = new Hono<{
 
 authRouter.post('/register', registerCompany);
 authRouter.post('/login', login);
-authRouter.get('/mfa/setup', verifyToken, setupMfa);
-authRouter.post('/mfa/verify', verifyToken, verifyMfaSetup);
+authRouter.get('/mfa/setup', verifyToken, onlyEmployee, setupMfa);
+authRouter.post('/mfa/verify', verifyToken, onlyEmployee, verifyMfaSetup);
