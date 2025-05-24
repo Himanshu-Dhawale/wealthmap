6dff432577d75ee23d47e5f2d6adc2fbf113efbc
chanchal16 <rajputchanchal17@gmail.com>
2025-05-24 22:23:32 +0530

    update: add fetch 2fa status endpoint

--- frontend/src/endpoints/auth.endpoint.ts
+++ frontend/src/endpoints/auth.endpoint.ts
@@ -2,3 +2,4 @@ export const SIGNIN = "/auth/login";
 export const SIGNUP = "/auth/register";
 export const MFA = "auth/mfa/setup";
 export const VERIFY_MFA = "auth/mfa/verify";
+export const GET_2FA_STATUS = "auth/mfa/status";
