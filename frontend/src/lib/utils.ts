db811bc6da6918e1a2c775284b5a4317645c04f4
chanchal16 <rajputchanchal17@gmail.com>
2025-05-24 11:09:40 +0530

    feat: add generate random color util function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -27,3 +27,17 @@ export const fileToBase64 = (file: File): Promise<string> => {
 export const loggingInUser = async (payload: SignInPayload) => {
   return await signIn("credentials", payload);
 };
+
+const hexCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F",];
+const getCharacter = (index: number) => {
+  return hexCharacters[index];
+};
+
+export const generateRandomColor = () => {
+  let hexColorPrefix = "#";
+  for (let index = 0; index < 6; index++) {
+    const randomPosition = Math.floor(Math.random() * hexCharacters.length);
+    hexColorPrefix += getCharacter(randomPosition);
+  }
+  return hexColorPrefix;
+};
