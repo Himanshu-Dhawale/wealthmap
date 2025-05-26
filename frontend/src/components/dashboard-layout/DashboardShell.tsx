f6b92688aafe08e063b4677fdb72b73100241d31
chanchal16 <rajputchanchal17@gmail.com>
2025-05-26 18:33:39 +0530

    update: add bookmark toggle

--- frontend/src/components/dashboard-layout/DashboardShell.tsx
+++ frontend/src/components/dashboard-layout/DashboardShell.tsx
@@ -1,14 +1,19 @@
+"use client";
 import { ReactNode } from "react";
 import DashboardHeader from "./DashboardHeader";
 import DashboardSidebar from "./DashboardSidebar";
+import { useMapStore } from "@/stores/mapStore";
+import { BookmarksSidebar } from "../map-interface/BookmarksSidebar";
 
 const DashboardShell = ({ children }: { children: ReactNode }) => {
+  const { showBookmarks } = useMapStore();
   return (
     <div className="flex h-screen">
       <DashboardSidebar />
       {/* Main Content */}
       <div className="flex-1 flex flex-col overflow-hidden">
         <DashboardHeader />
+        {showBookmarks && <BookmarksSidebar />}
         <main className="flex-1 overflow-y-auto p-6 bg-gray-50">
           {children}
         </main>
