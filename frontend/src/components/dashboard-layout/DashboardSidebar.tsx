cb28f4717053121a82fdcb04af053dc63a5526bf
chanchal16 <rajputchanchal17@gmail.com>
2025-05-26 23:32:40 +0530

    update: add home link

--- frontend/src/components/dashboard-layout/DashboardSidebar.tsx
+++ frontend/src/components/dashboard-layout/DashboardSidebar.tsx
@@ -8,6 +8,7 @@ import {
   Users,
   Settings,
 } from "lucide-react";
+import Link from "next/link";
 
 const DashboardSidebar = () => {
   const { data, status } = useSession();
@@ -16,9 +17,12 @@ const DashboardSidebar = () => {
     <nav className="hidden md:flex md:flex-shrink-0">
       <div className="flex flex-col w-64 bg-white border-r border-gray-200">
         <div className="flex items-center h-16 px-6 border-b border-gray-200">
-          <span className="text-xl font-bold text-transparent bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text">
+          <Link
+            href={"/"}
+            className="text-xl font-bold text-transparent bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text"
+          >
             WealthMap
-          </span>
+          </Link>
         </div>
         <div className="flex-1 py-4 overflow-y-auto">
           <nav className="px-4 space-y-1">
