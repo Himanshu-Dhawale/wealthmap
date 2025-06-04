5d92d291f0b06db4b3d9dd9f24111da36ecd721a
Sharmaryan <panditaryan.as@gmail.com>
2025-06-04 16:19:25 +0530

    refactor : updated email utility

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 
-export async function sendInvitationEmail(to: string, invitationLink: string) {
+export async function sendEmail(to: string, subject: string, html: string) {
 	const transporter = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
@@ -13,14 +13,11 @@ export async function sendInvitationEmail(to: string, invitationLink: string) {
 		const info = await transporter.sendMail({
 			from: `"WealthMap" <${process.env.GMAIL_USER}>`,
 			to,
-			subject: "You're invited to join the company on WealthMap!",
-			html: `
-		  <p>You've been invited to join WealthMap.</p>
-		  <p>Click <a href="${invitationLink}">here</a> to accept the invitation.</p>
-		`,
+			subject,
+			html,
 		});
 		console.info('Email sent:', info.messageId);
-		return { success: true, message: 'Invitation sent successfully!' };
+		return { success: true, message: 'Email sent successfully' };
 	} catch (error) {
 		console.error('Email sending error:', error);
 		return { success: false, error, message: 'Failed to send email' };
